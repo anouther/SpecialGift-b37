0c0bfeab15f3359427d2ebb908ab4799db89bf3d
anouther <155570107+anouther@users.noreply.github.com>
2025-07-17 21:05:38 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Together, we’re unstoppable. Gavie, you’re my favorite adventure and my sweetest love";
+            subtitle.innerText = "happy 2nd mensive, sayang. two months might sound small, but for me it’s enough to know how much you mean to me. kadang aku mikir, gimana kalo aku gak ketemu kamu? pasti hidupku bakal sepi banget. thank you for staying, even when i’m hard to deal with. you matter, more than you’ll ever know.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
